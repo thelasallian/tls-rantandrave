dc2fd7d2b27362a03255b1c1fafb0aa46f5af391
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-08-23 23:13:15 +0800

    Added authors and content to WordPress urls

--- php/session/session-home.php
+++ php/session/session-home.php
@@ -3,11 +3,11 @@
 /*  Wordpress Endpoints for 4 most recent articles per tag,
  *  5 most recent articles for $wp_url_all
  */
-$wp_url_all   = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,excerpt&tags=497&per_page=5"; // All RNR articles
-$wp_url_movie = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags&tags=2147&per_page=4"; // 'Movie' tag
-$wp_url_tv    = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags&tags=2225&per_page=4"; // 'Television' tag
-$wp_url_music = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags&tags=2107&per_page=4"; // 'Music' tag
-$wp_url_misc  = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags&tags=2226&per_page=4"; // 'Miscellaneous' tag
+$wp_url_all   = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,excerpt,content&,authorstags=497&per_page=5"; // All RNR articles
+$wp_url_movie = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2147&per_page=4"; // 'Movie' tag
+$wp_url_tv    = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2225&per_page=4"; // 'Television' tag
+$wp_url_music = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2107&per_page=4"; // 'Music' tag
+$wp_url_misc  = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2226&per_page=4"; // 'Miscellaneous' tag
 
 /* Code by Rafael Gabriel Arceo */
 if (session_status() == PHP_SESSION_NONE) {
