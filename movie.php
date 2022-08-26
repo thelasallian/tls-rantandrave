eea0354946cc57cae2f2fe4cef59b125d40eb010
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-08-26 22:33:19 +0800

    Added movie.php

---
+++ movie.php
@@ -0,0 +1,24 @@
+<?php $url = ""; ?>
+<?php require_once 'php/functions.php' ?>
+<?php require_once 'php/session/session-subpages.php' ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require_once 'php/components/head.php' ?>
+    <link rel="stylesheet" href="css/subpages.css">
+
+    <!-- Title -->
+    <title>The LaSallian: Rant and Rave - Movie</title>
+</head>
+
+<body>
+    
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
