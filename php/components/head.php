038da9ba61ee2b4ca726bffda2acdb5b51f30ecb
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-08-26 22:06:35 +0800

    Removed duplicate <link> tags

--- php/components/head.php
+++ php/components/head.php
@@ -8,8 +8,6 @@
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&family=Work+Sans:wght@400;700&display=swap" rel="stylesheet"> 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
