1308a6c4ab94c7550dd57aacb83323b18b1a5fd9
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-08-26 22:53:02 +0800

    Added require_once() for navbar and footer

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 </head>
 
 <body>
+    <!-- Navbar -->
+    <?php require_once 'php/components/navbar.php' ?>
+
     <!-- Header -->
     <header>
         <h1>Header</h1>
@@ -33,6 +36,8 @@
     }
     ?>
 
+    <!-- Footer -->
+    <?php require_once 'php/components/footer.php' ?>
 
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
--- movie.php
+++ movie.php
@@ -14,8 +14,12 @@
 </head>
 
 <body>
-    
+    <!-- Navbar -->
+    <?php require_once 'php/components/navbar.php' ?>
 
+
+    <!-- Footer -->
+    <?php require_once 'php/components/footer.php' ?>
 
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
