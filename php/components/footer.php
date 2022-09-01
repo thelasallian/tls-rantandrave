55608b08e1b5ee8084b112de7415243fd00913b3
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-09-02 00:45:11 +0800

    Added footer skeleton

---
+++ php/components/footer.php
@@ -0,0 +1,31 @@
+<footer class="py-5">
+        <div class="container">
+            <div class="row">
+                <!-- Left Side: TLS and Site Name -->
+                <div class="col-sm-12 col-lg-6 d-flex justify-content-center align-self-center align-items-lg-start text-center">
+                    <figure>
+                        <!-- Logo -->
+                        <img src="assets/tls-logo-start-white.png" alt="TLS Logo" class="footer-logo mb-4 img-fluid">
+                        <!-- Logo Heading -->
+                        <figcaption class="footer-heading">Menagerie</figcaption>
+                        <figcaption class="footer-heading">Rant & Rave</figcaption>
+                    </figure>
+                </div>
+                <!-- Description -->
+                <div class="col-sm-12 col-lg-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
+                    <!-- Description -->
+                    <p class="footer-description">
+                        <strong>The LaSallian</strong> is the official student publication of De La Salle University.
+                        It is of the students, by the students, and for the students. Our student writers, photographers,
+                        videographers, and web managers are committed to the 61-year tradition of journalistic excellence
+                        and issue-oriented critical thinking.
+                    </p>
+                    <!-- Link to Main Website -->
+                    <p class="footer-link-main-site"><a href="https://thelasallian.com"><strong>Go to Main Website →</strong></a></p>
+                    <!-- Website Developers -->
+                    <p class="footer-developers mt-3">Copyright 2022 © <strong>The LaSallian</strong></p>
+                    <p class="footer-developers mt-3">Website developed by <a href="https://github.com/angelocguerra" class="fs-5" target="_blank">Angelo Guerra</a> and <a href="https://github.com/ronnparcia" class="fs-5" target="_blank">Ronn Parcia</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
