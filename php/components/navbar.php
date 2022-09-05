4a624ebb0d4971b21f60953d3df57529397aaa80
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-09-05 21:41:11 +0800

    Added basic responsive navbar

---
+++ php/components/navbar.php
@@ -0,0 +1,50 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container d-flex justify-content-between">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            <img src="assets/tls-logo-star-white.png" style="width: 4rem; " alt="">
+        </a>
+
+        <div class="d-flex flex-grow-1 justify-content-end justify-content-md-center">
+            <!-- Menu Button for Mobile Devices -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <!-- Links -->
+            <div class="offcanvas offcanvas-end flex-grow-0 text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Rant and Rave</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active text-reset" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-reset" href="#">Movie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-reset" href="#">Television</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-reset" href="#">Music</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-reset" href="#">Miscellaneous</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Search Icons -->
+        <div class="d-flex justify-content-end" style="width: 4rem;">
+            <span class="material-icons text-white">search</span>
+        </div>
+    </div>
+</nav>
