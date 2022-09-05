fbf2999abe10d37fbfc2ad52a9e7479a2999a17f
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-09-05 22:10:13 +0800

    Turned search icon into modal button and added modal for search

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -41,12 +41,31 @@
                 </div>
 
             </div>
-            
+
         </div>
 
         <!-- Search Icons -->
         <div class="d-flex justify-content-end" style="width: 4rem;">
-            <span class="material-icons text-white">search</span>
+            <!-- Button trigger Search modal -->
+            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#searchModal">
+                <span class="material-icons text-white">search</span>
+            </button>
+
+            <!-- Search Modal -->
+            <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="searchModalLabel">Search Article</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 </nav>
