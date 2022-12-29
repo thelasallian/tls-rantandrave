948bcf0f61a59015f5f4a68f499c6e7a5f585cec
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-12-29 21:58:20 +0800

    Article title and date padding, remove bg color and padding on mobile

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ function render_article_cards($articles, $article_rating_class, $article_title_c
                 <span class="ac-rating d-inline-block rounded-5 mb-3 {$article_rating_class}">{$rating}</span>
                 
                 <!-- Article Title and Date -->
-                <div class="d-block rounded-3 p-3 mb-3 {$article_title_class}">
+                <div class="ac-title-date d-block rounded-3 mb-3 {$article_title_class}">
                     <h2 class="ac-title mb-2">{$title}</h2>
                     <span class="ac-date fs-7">{$date}</span>
                 </div>
