686fe332bb59959f0b46d71b1732899af4faefd6
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-02 00:41:21 +0800

    Coding convention

--- php/functions/functions-search-results.php
+++ php/functions/functions-search-results.php
@@ -4,9 +4,15 @@
 
 // TODO: Add documentation
 // TODO: Rename this function name (and across other files where it is called) to reflect that it's for search results only
-function initialize_article_info($article, &$visual_url, &$title, &$date,
-                                 &$authors, &$article_url, &$content)
-{
+function initialize_article_info(
+    $article, 
+    &$visual_url, 
+    &$title, 
+    &$date,
+    &$authors,
+    &$article_url,
+    &$content
+) {
     $visual_url = $article["jetpack_featured_media_url"];
     $title = del_kicker($article["title"]["rendered"]);
     $date = date('F j, Y', strtotime($article["date"]));
