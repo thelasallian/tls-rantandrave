2db7fa19791a5c7c1092826425dae7a05450830e
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-02 00:42:34 +0800

    Function documentation

--- php/functions/functions-search-results.php
+++ php/functions/functions-search-results.php
@@ -2,8 +2,18 @@
 <!-- Functions Specific to Search Results -->
 <?php
 
-// TODO: Add documentation
 // TODO: Rename this function name (and across other files where it is called) to reflect that it's for search results only
+/**
+ * This function assigns the article information to individual variables
+ * 
+ * @param $article      an array containing the article's information
+ * @param &$visual_url  the reference to the variable which will be assigned the article visual url
+ * @param &$title       the reference to the variable which will be assigned the article title
+ * @param &$date        the reference to the variable which will be assigned the article date
+ * @param &$authors     the reference to the variable which will be assigned the article authors
+ * @param &$article_url the reference to the variable which will be assigned the article url
+ * @param &$content     the reference to the variable which will be assigned the article body. used for searching keywords
+ */
 function initialize_article_info(
     $article, 
     &$visual_url, 
