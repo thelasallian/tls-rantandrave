0df951a9eb0170007d188de70af5604f4125a835
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-01 22:32:04 +0800

    Added documentation

--- php/functions/functions-category-subpages.php
+++ php/functions/functions-category-subpages.php
@@ -1,10 +1,21 @@
-<!-- TODO: Add description for this file -->
-<!-- Functions specific to category subpages (Movie, TV, Music, Miscellaneous) -->
+<!-- 
+    This files contains functions that are used only in the category subpages,
+    namely movie.php, television.php, music.php, and miscellaneous.php
+ -->
 
 <?php 
 
-// TODO: Add documentation
 // TODO: Rename this function name (and across other files where it is called) to reflect that it's for category subpages only
+/**
+ * This function assigns the article information to individual variables
+ * 
+ * @param $article      an array containing the article's information
+ * @param &$visual_url  the reference to the variable which will be assigned the article visual url
+ * @param &$title       the reference to the variable which will be assigned the article title
+ * @param &$date        the reference to the variable which will be assigned the article date
+ * @param &$authors     the reference to the variable which will be assigned the article authors
+ * @param &$article_url the reference to the variable which will be assigned the article url
+ */
 function initialize_article_info($article, &$visual_url, &$title,
                                  &$date, &$authors, &$article_url)
 {
