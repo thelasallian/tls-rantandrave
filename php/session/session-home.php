2b46b1ba1f07b337d4bc35d03d4a2bc92e3dc10e
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-01-09 23:55:43 +0800

    Edited Wordpress Fetch Link

--- php/session/session-home.php
+++ php/session/session-home.php
@@ -3,7 +3,7 @@
 /*  Wordpress Endpoints for 4 most recent articles per tag,
  *  5 most recent articles for $wp_url_all
  */
-$wp_url_all   = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,excerpt,content&,authorstags=497&per_page=5"; // All RNR articles
+$wp_url_all   = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,excerpt,content,authors&tags=497&per_page=5"; // All RNR articles
 $wp_url_movie = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2147&per_page=4"; // 'Movie' tag
 $wp_url_tv    = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2225&per_page=4"; // 'Television' tag
 $wp_url_music = "https://thelasallian.com/wp-json/wp/v2/posts?_fields=title,link,jetpack_featured_media_url,date,tags,content,authors&tags=2107&per_page=4"; // 'Music' tag
