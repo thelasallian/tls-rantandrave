5fb557c80f234b20b394ce35ee7f00e37cbf8915
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-09 20:43:10 +0800

    Changed modal header

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -57,7 +57,7 @@
                     <div class="modal-content">
                         <!-- Modal Header -->
                         <div class="modal-header">
-                            <h5 class="modal-title" id="searchModalLabel">What article are you looking for?</h5>
+                            <h5 class="modal-title" id="searchModalLabel">Search Rant and Rave articles</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <!-- Modal Body -->
