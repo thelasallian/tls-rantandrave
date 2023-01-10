82bb880e8b00de1cd9bb87c14561ef69441f8cbc
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-10 20:55:41 +0800

    Renamed and moved init_article_info_subpage to functions-subpages.php

--- php/functions/functions-subpages.php
+++ php/functions/functions-subpages.php
@@ -41,5 +41,30 @@ function render_page_links($page_count, $current_url) {
         PAGE_LINK;
     }
 }
+
+/**
+ * This function assigns the article information to individual variables
+ * 
+ * @param $article      an array containing the article's information
+ * @param &$visual_url  the reference to the variable which will be assigned the article visual url
+ * @param &$title       the reference to the variable which will be assigned the article title
+ * @param &$date        the reference to the variable which will be assigned the article date
+ * @param &$authors     the reference to the variable which will be assigned the article authors
+ * @param &$article_url the reference to the variable which will be assigned the article url
+ */
+function init_article_info_subpage(
+    $article,
+    &$visual_url,
+    &$title,
+    &$date,
+    &$authors,
+    &$article_url
+) {
+    $visual_url = $article["jetpack_featured_media_url"];
+    $title = del_kicker($article["title"]["rendered"]);
+    $date = date('F j, Y', strtotime($article["date"]));
+    $authors = get_authors($article["authors"]);
+    $article_url = $article["link"];
+}
  
  ?>
