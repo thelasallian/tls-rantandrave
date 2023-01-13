92dbbbfa4b0d62c6dee86a50034b824622a2542a
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-13 23:42:08 +0800

    Make navbar translucent dark on scroll

---
+++ js/navbar-subpages.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar").style.height = "4rem";
+        document.getElementById("nav-logo").style.width = "2.5rem";
+        // Change CSS style
+        document.getElementById("navbar").classList.add("nav-darkbg");
+        document.getElementById("navbar").classList.remove("nav-hollowbg");
+    } else {
+        document.getElementById("navbar").style.height = "5.75rem";
+        document.getElementById("nav-logo").style.width = "3rem";
+        // Change CSS style
+        document.getElementById("navbar").classList.remove("nav-darkbg");
+        document.getElementById("navbar").classList.add("nav-hollowbg");
+    }
+}
