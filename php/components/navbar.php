0b956f006648bb6d4f927b3c001df7d86983a78a
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2023-01-13 23:28:48 +0800

    Moved JS scripts to separate files

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -60,18 +60,3 @@
 
     </div>
 </nav>
-
-<!-- Script to Make Navbar Shrink on Scroll -->
-<script>
-    window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("navbar").style.height = "4rem";
-        document.getElementById("nav-logo").style.width = "2.5rem";
-    } else {
-        document.getElementById("navbar").style.height = "5.75rem";
-        document.getElementById("nav-logo").style.width = "3rem";
-    }
-    }
-</script>
